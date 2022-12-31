48399f413021ed8855d5525f9af37a86e3254161
Marc Meynet <marcmeynet@gmail.com>
2022-12-31 08:28:31 +0100

    (feat) crated landing setion

--- layout/LandingSection.tsx
+++ layout/LandingSection.tsx
@@ -1,13 +1,21 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
 import Section from "../components/UI/Section";
 
 const LandingSection = () => {
   return (
     <Section>
-      <Box>
-        <Heading as="h1">Marc Meynet</Heading>
-        <Heading as="h2">Full-stack web developer from Switzerland</Heading>
-        <Box h="1500px" w="32px" bg="red" pos="absolute" />
+      <Box mt="20%">
+        <Heading as="h1" fontWeight={600} fontSize="64px">
+          Marc Meynet
+        </Heading>
+        <Flex align="center" gap={4} mt={2}>
+          <Heading as="h2" fontSize="32px" fontWeight={300}>
+            Full-stack web developer from Switzerland
+          </Heading>
+          <Image src="img/swissflag.png" alt="Swiss flag" w="26px" h="26px" />
+        </Flex>
+
+        <Box h="1500px" w="32px" />
       </Box>
     </Section>
   );
